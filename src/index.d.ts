2364d913bdd7dabfbb0c0620ae6cd869b6eb1772
elias.souza <elias.souza@biosistemico.com.br>
2023-08-28 11:39:56 -0300

    feat: set resolver options to be optional

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,7 @@
 import { ResolverOptions } from "./types/ResolverOptions";
 
 export { ResolverOptions }
-export default function useFetcher<Data = any, Error = any>(key: string, promise: Promise<any>, options?: ResolverOptions): {
+export default function useFetcher<Data = any, Error = any>(key: string, promise: Promise<any>, options?: Partial<ResolverOptions>): {
   isLoading: boolean;
   data: Data | undefined;
   error: Error | undefined;
